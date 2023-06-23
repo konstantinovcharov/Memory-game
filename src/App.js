854d50d2d81d16ace1722bb6e6cd801c96ec2e9e
Konstantin Ovcharov <konstantinovcharov@abv.bg>
2023-06-23 15:01:33 +0300

    new images and message when game is won

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import './App.css'
 import SingleCard from './components/SingleCard'
 
 const cardImages = [
-  { "src": "/img/helmet-1.png", matched: false },
-  { "src": "/img/potion-1.png", matched: false },
-  { "src": "/img/ring-1.png", matched: false },
-  { "src": "/img/scroll-1.png", matched: false },
-  { "src": "/img/shield-1.png", matched: false },
-  { "src": "/img/sword-1.png", matched: false },
+  { "src": "/img/helmet2.png", matched: false },
+  { "src": "/img/potion2.png", matched: false },
+  { "src": "/img/ring2.png", matched: false },
+  { "src": "/img/scroll2.png", matched: false },
+  { "src": "/img/shield2.png", matched: false },
+  { "src": "/img/sword2.png", matched: false }
 ]
 
 function App() {
-
   const [cards, setCards] = useState([])
   const [turns, setTurns] = useState(0)
   const [choiceOne, setChoiceOne] = useState(null)
   const [choiceTwo, setChoiceTwo] = useState(null)
   const [disabled, setDisabled] = useState(false)
+  const [gameCompleted, setGameCompleted] = useState(false) 
 
   // shuffle cards for new game
   const shuffleCards = () => {
@@ -26,30 +26,34 @@ function App() {
       .map(card => ({ ...card, id: Math.random() }))
 
     setChoiceOne(null)
-    setChoiceTwo(null)  
+    setChoiceTwo(null)
     setCards(shuffledCards)
     setTurns(0)
+    setGameCompleted(false) 
   }
 
   // handle a choice
   const handleChoice = (card) => {
-    console.log(card)
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
   }
-
 
   useEffect(() => {
     if (choiceOne && choiceTwo) {
       setDisabled(true)
       if (choiceOne.src === choiceTwo.src) {
         setCards(prevCards => {
-          return prevCards.map(card => {
+          const updatedCards = prevCards.map(card => {
             if (card.src === choiceOne.src) {
               return { ...card, matched: true }
             } else {
               return card
             }
           })
+          
+          if (updatedCards.every(card => card.matched)) {
+            setGameCompleted(true)
+          }
+          return updatedCards
         })
         resetTurn()
       } else {
@@ -58,8 +62,6 @@ function App() {
     }
   }, [choiceOne, choiceTwo])
 
-  console.log(cards)
-
   const resetTurn = () => {
     setChoiceOne(null)
     setChoiceTwo(null)
@@ -67,11 +69,13 @@ function App() {
     setDisabled(false)
   }
 
-  useEffect(() => {shuffleCards()}, [])
+  useEffect(() => {
+    shuffleCards()
+  }, [])
 
   return (
     <div className="App">
-      <h1>Memory Card Game</h1>
+      
       <button onClick={shuffleCards}>New Game</button>
       <div className="card-grid">
         {cards.map(card => (
@@ -80,10 +84,12 @@ function App() {
             card={card}
             handleChoice={handleChoice}
             flipped={card === choiceOne || card === choiceTwo || card.matched}
-            disabled={disabled} />
+            disabled={disabled}
+          />
         ))}
       </div>
       <p>Turns: {turns}</p>
+      {gameCompleted && <p>Congratulations! You won!</p>}
     </div>
   );
 }
